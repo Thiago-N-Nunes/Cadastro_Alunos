8928c6b44b3d67e21e720437bb0d8ebb2c031385
Thiago Nunes <thiagonesi.cntt@hotmail.com>
2026-04-29 21:23:13 -0300

    base do programa estruturado

---
+++ app.py
@@ -0,0 +1,16 @@
+from modulos import funcoes
+
+while True:
+    print('Cadastro de alunos')
+    opcoes = int(input('\n1 - Cadastrar Aluno\n2 - Listar Alunos\n3 - Alterar dados\n4 - Remover Aluno\n5 - Fechar'))
+    match opcoes:
+        case 1:
+            ...
+        case 2:
+            ...
+        case 3:
+            ...
+        case 4:
+            ...
+        case 5:
+            break
